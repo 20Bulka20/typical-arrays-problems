390a298ec8adc5fcb826215552e651e8dcb56d59
20Bulka20 <polina.rozhko.blr@gmail.com>
2020-02-29 15:32:03 +0300

    feat: add my sollution to Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
+// There are a few typical problems while you working with arrays, and your goal is to solve it.
+// You should implement 3 functions: max, min, and avg for searching minimal value, 
+//maximal value, and an average value of a given array.
+// For example:
+// min(1, 2, 3, 4) // returns 1;
+// max(1, 2, 3, 4) // returns 4;
+// avg(1, 2, 3, 4) // returns 2.5;
+// Please note, that you should return 0 if no params specified or an empty array passed
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array)){
+  if(array.length==0) return 0;
+  else {
+    array.sort(function(a, b) {
+      return a - b;
+    });
+    return array[0];
+  } }
+  else return 0;
 }
-
 exports.max = function max (array) {
-  return 0;
+  if(Array.isArray(array)){
+  if(array.length==0) return 0;
+  else {
+    array.sort(function(a, b) {
+      return b - a;
+    });
+    return array[0];
+  }}
+  else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(Array.isArray(array)){
+  if(array.length==0) return 0;
+  else{    
+    let s = array.reduce((sum,cur)=> sum+cur,0 );
+    return s/array.length;
+  }}
+  else return 0;
 }
